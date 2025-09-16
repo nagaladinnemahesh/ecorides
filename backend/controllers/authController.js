c6857a61c3388f008d5db34e7a802d65051fea5f
MaheshNagaladinne <nagaladinnemahesh07@gmail.com>
2025-09-16 15:44:49 +0530

    added protected routes trail

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,23 +44,36 @@ const loginUser = async(req, res) => {
     try {
         const {email, password} = req.body;
 
+        //checking user if exists
+
         const user = await User.findOne({email});
 
-        if (user && (await bcrypt.compare(password, user.password))){
-            res.json({
-                _id: user.id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-                token: generateToken(user._id),
-            });
-        } else {
-            res.status(400).json({message: "Invalid Credentials"});
+        if (!user){
+            return res.status(400).json({message: "Invalid Credentials"});
         }
+
+        //check password match
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+
+        // send response with token
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id)
+        });
+
     } catch (error){
         res.status(500).json({message: error.message});
     }
-};
+}
+
 
 // Generate JWT
 
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const {registerUser, loginUser} = require("../controllers/authController");
-
 const router = express.Router();
+const {protect} = require("../middleware/authMiddleware");
 
-// Register user
+// Public Routes
 
 router.post("/register", registerUser);
-
-// Login user
-
 router.post("/login", loginUser);
+
+// trail for protected route
+
+router.get("/profile", protect, (req, res) => {
+    res.json({
+        message: "Protected route accessed",
+        user: req.user,
+    })
+})
+
 
 module.exports = router;
